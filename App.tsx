ba4803b81c95344d26d767a8afc587d8c3d5939c
Chris Villarroel <chrisdev3001@gmail.com>
2024-09-06 12:19:48 -0400

    se agregan rutas absolutas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { Spinner } from '@core'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hola mundo desde app usuarios</Text>
       <StatusBar style='auto' />
+      <Spinner />
     </View>
   )
 }
