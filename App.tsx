d221f16b92b7a16969a8b09eeea98b9cff36d7ce
Chris Villarroel <chrisdev3001@gmail.com>
2024-09-08 17:59:09 -0300

    se agregan fuentes a nuestra app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar'
+import * as SplashScreen from 'expo-splash-screen'
 import { StyleSheet, Text, View } from 'react-native'
 import { Spinner } from '@core'
+import { useFonts } from 'expo-font'
+import { useEffect } from 'react'
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    'PlaypenSans-Bold': require('./assets/fonts/PlaypenSans-Bold.ttf'),
+    'PlaypenSans-Light': require('./assets/fonts/PlaypenSans-Light.ttf'),
+    'Regular-Bold': require('./assets/fonts/PlaypenSans-Regular.ttf'),
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hola mundo desde app usuarios</Text>
+      <Text style={{ fontFamily: 'PlaypenSans-Bold', fontSize: 24 }}>
+        Hola mundo desde app usuarios
+      </Text>
+      <Text style={{ fontFamily: 'PlaypenSans-Regular', fontSize: 24 }}>
+        Hola mundo desde app usuarios
+      </Text>
+      <Text style={{ fontFamily: 'PlaypenSans-Light', fontSize: 24 }}>
+        Hola mundo desde app usuarios
+      </Text>
       <StatusBar style='auto' />
       <Spinner />
     </View>
