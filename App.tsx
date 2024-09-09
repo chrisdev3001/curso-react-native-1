3a7b49159efb326e72d7dd7c064245f23bc63aa2
Chris Villarroel <chrisdev3001@gmail.com>
2024-09-09 13:14:12 -0300

    se agrega componente Button

--- App.tsx
+++ App.tsx
@@ -1,16 +1,7 @@
 import { StatusBar } from 'expo-status-bar'
 import * as SplashScreen from 'expo-splash-screen'
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  ActivityIndicator,
-  TextInput,
-  Alert,
-  TouchableOpacity,
-} from 'react-native'
-import { COLORS, Spinner } from '@core'
+import { StyleSheet, View } from 'react-native'
+import { Button } from '@core'
 import { useFonts } from 'expo-font'
 import { useEffect } from 'react'
 
@@ -33,21 +24,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Button title='Presioname!' onPress={() => Alert.alert('Hola mundo')} />
-      <TouchableOpacity
-        style={{
-          backgroundColor: COLORS.primary,
-          padding: 16,
-          borderRadius: 5,
-          marginTop: 10,
-        }}
-        onPress={() => Alert.alert('Hola mundo')}>
-        <Text style={{ color: COLORS.white, fontSize: 24 }}>
-          Hola soy un botón custom
-        </Text>
-      </TouchableOpacity>
-      <ActivityIndicator size='large' color='blue' />
-      <TextInput style={{ backgroundColor: 'pink', width: 300, padding: 16 }} />
+      <Button title='Hello' onPress={() => console.log('Hello')} />
     </View>
   )
 }
